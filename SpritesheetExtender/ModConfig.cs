f28a4034f4dca0c18c54b30933d123d29019728c
Caleb Gaffney <ioicaleb12@gmail.com>
2024-09-20 08:46:33 -0500

    Add defulat value to Config

--- SpritesheetExtender/ModConfig.cs
+++ SpritesheetExtender/ModConfig.cs
@@ -6,7 +6,7 @@ namespace SpritesheetExtender
     {
         public int[][] OffsetsFemale { get; set; }
         public int[][] OffsetsMale { get; set; }
-        public int NumSpritesAddedFemale { get; set; }
-        public int NumSpritesAddedMale { get; set; }
+        public int NumSpritesAddedFemale { get; set; } = 0;
+        public int NumSpritesAddedMale { get; set; } = 0;
     }
 }
